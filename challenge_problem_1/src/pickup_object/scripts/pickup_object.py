94fad75e7b7c7b0cfeb76b1fcec09a8cfc9addbc
hariravi <hariravi7@gmail.com>
2015-04-01 00:25:39 -0400

    Small test main in python

--- challenge_problem_1/src/pickup_object/scripts/pickup_object.py
+++ challenge_problem_1/src/pickup_object/scripts/pickup_object.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
 
     #initialize the node
     rospy.init_node('pickup_object', anonymous=True)
-
-    #sleep for a moment while everything starts up
-    rospy.sleep(1.0)
-
-    # your code here:
-    rospy.loginfo("Finished Picking up the cup!")
+    move_base = rospy.ServiceProxy('move_base', '/../../move_base/srv/MoveBase.srv' )
+    try:
+        mb = move_base(.75)
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
+    
+    rospy.loginfo("Let's see if this works")
