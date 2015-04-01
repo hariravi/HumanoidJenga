4805549b46b5e2330b1a7c63c99bf22be6261648
hariravi <hariravi7@gmail.com>
2015-04-01 10:59:54 -0400

    Try in clic lab

--- challenge_problem_1/src/pickup_object/scripts/pickup_object.py
+++ challenge_problem_1/src/pickup_object/scripts/pickup_object.py
@@ -7,10 +7,22 @@ if __name__ == '__main__':
 
     #initialize the node
     rospy.init_node('pickup_object', anonymous=True)
+    # Move base sample call
+    rospy.wait_for_service('move_base')
     move_base = rospy.ServiceProxy('move_base', '/../../move_base/srv/MoveBase.srv' )
+    distance = .75
     try:
-        mb = move_base(.75)
+        mb = move_base(distance)
     except rospy.ServiceException as exc:
         print("Service did not process request: " + str(exc))
     
+    # Rotate base sample call
+    rospy.wait_for_service('rotate_base')
+    move_base = rospy.ServiceProxy('rotate_base', '/../../move_base/srv/RotateBase.srv' )
+    # Angle in radians
+    angle = 3.14
+    try:
+        rb = rotate_base(distance)
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
     rospy.loginfo("Let's see if this works")
